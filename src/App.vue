7d15e49a405cf74a705da21fbd039f990e287283
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-12 14:32:20 -0500

    feat: create ResponseBox component

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,11 @@
       <h1 class="text-white text-center font-bold text-2xl">IP Address Tracker</h1>
     </header>
     <IpInput />
+    <ResponseBox />
   </main>
 </template>
 
 <script lang="ts" setup>
 import IpInput from './components/IpInput.vue';
+import ResponseBox from './components/ResponseBox.vue';
 </script>
---
+++ src/components/ResponseBox.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="bg-white flex flex-col justify-center space-y-5 py-5 rounded-xl">
+    <div class="flex flex-col justify-center space-y-1">
+      <span class="text-center text-xs text-gray-500 font-bold uppercase">Ip Address</span>
+      <span class="text-center text-2xl text-gray-800 font-bold">192.212.174.101</span>
+    </div>
+    <div class="flex flex-col justify-center space-y-1">
+      <span class="text-center text-xs text-gray-500 font-bold uppercase">Location</span>
+      <span class="text-center text-2xl text-gray-800 font-bold">Brooklyn, NY 10001</span>
+    </div>
+    <div class="flex flex-col justify-center space-y-1">
+      <span class="text-center text-xs text-gray-500 font-bold uppercase">Timezone</span>
+      <span class="text-center text-2xl text-gray-800 font-bold">UTC -05:00</span>
+    </div>
+    <div class="flex flex-col justify-center space-y-1">
+      <span class="text-center text-xs text-gray-500 font-bold uppercase">Isp</span>
+      <span class="text-center text-2xl text-gray-800 font-bold">SpaceX Starlink</span>
+    </div>
+  </div>
+</template>
