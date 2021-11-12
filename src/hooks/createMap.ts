ec0f80876df41def3a7edfbb7d67975381bf1248
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-12 17:34:57 -0500

    refactor: move create map hook to its own file

---
+++ src/hooks/createMap.ts
@@ -0,0 +1,33 @@
+import { Ref } from 'vue';
+import Leaflet from 'leaflet';
+
+const svgIcon = Leaflet.divIcon({
+  html: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="46"
+        height="56"
+        
+      >
+        <path
+          fill-rule="evenodd"
+          d="M39.263 7.673c8.897 8.812 8.966 23.168.153 32.065l-.153.153L23 56 6.737 39.89C-2.16 31.079-2.23 16.723 6.584 7.826l.153-.152c9.007-8.922 23.52-8.922 32.526 0zM23 14.435c-5.211 0-9.436 4.185-9.436 9.347S17.79 33.128 23 33.128s9.436-4.184 9.436-9.346S28.21 14.435 23 14.435z"
+        />
+      </svg>`,
+  className: 'svg-icon',
+  iconSize: [46, 56],
+  iconAnchor: [12, 40],
+});
+
+export const createMap = (map: Ref<Leaflet.Map | undefined>): void => {
+  map.value = Leaflet.map('map', {
+    zoomControl: false,
+  }).setView([10.39972, -75.51444], 15);
+
+  Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map.value);
+
+  Leaflet.marker([10.39972, -75.51444], { icon: svgIcon }).addTo(map.value);
+};
