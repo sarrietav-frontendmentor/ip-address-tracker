6ddd104a92f0f39dea1146538910bee670efc443
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-12 12:45:04 -0500

    refactor: make IpInput a component

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,10 @@
     <header>
       <h1 class="text-white text-center font-bold text-2xl">IP Address Tracker</h1>
     </header>
-    <form class="flex items-stretch">
-      <input type="text" class="rounded-tl-xl rounded-bl-xl flex-grow" />
-      <button type="submit" class="inline bg-black p-5 rounded-tr-xl rounded-br-xl">
-        <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
-          <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
-        </svg>
-      </button>
-    </form>
+    <IpInput />
   </main>
 </template>
+
+<script lang="ts" setup>
+import IpInput from './components/IpInput.vue';
+</script>
---
+++ src/components/IpInput.vue
@@ -0,0 +1,10 @@
+<template>
+  <form class="flex items-stretch">
+    <input type="text" class="rounded-tl-xl rounded-bl-xl flex-grow" />
+    <button type="submit" class="inline bg-black p-5 rounded-tr-xl rounded-br-xl">
+      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14">
+        <path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6" />
+      </svg>
+    </button>
+  </form>
+</template>
