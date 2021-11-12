02577599f3d1ff987bef25b1aaeea5df790a9904
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-12 15:42:45 -0500

    feat: add map

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
-  <main class="w-full bg-pattern flex flex-col p-5 space-y-5">
-    <header>
-      <h1 class="text-white text-center font-bold text-2xl">IP Address Tracker</h1>
-    </header>
-    <IpInput />
-    <ResponseBox />
-  </main>
+  <div class="h-screen">
+    <main class="w-full bg-pattern flex flex-col p-5 space-y-5">
+      <header>
+        <h1 class="text-white text-center font-bold text-2xl">IP Address Tracker</h1>
+      </header>
+      <IpInput />
+      <ResponseBox />
+    </main>
+    <div id="map" class="w-full h-full"></div>
+  </div>
 </template>
 
 <script lang="ts" setup>
+import Leaflet from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import { ref, onMounted } from 'vue';
 import IpInput from './components/IpInput.vue';
 import ResponseBox from './components/ResponseBox.vue';
+
+const map = ref();
+
+onMounted(() => {
+  map.value = Leaflet.map('map').setView([10.39972, -75.51444], 12);
+  Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution:
+      '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  }).addTo(map.value);
+});
 </script>
