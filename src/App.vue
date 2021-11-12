ef14c37061dbfbef8dac99f141dfc53da5678f92
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-12 14:34:35 -0500

    fix(App): add vertical spacing

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <main class="w-full bg-pattern flex flex-col p-5">
+  <main class="w-full bg-pattern flex flex-col p-5 space-y-5">
     <header>
       <h1 class="text-white text-center font-bold text-2xl">IP Address Tracker</h1>
     </header>
