461dc028af796f552a087807d83950453e8f5c4a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-12 21:49:52 -0500

    style(eslintrc): format

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,11 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/typescript/recommended'],
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 2020,
   },
