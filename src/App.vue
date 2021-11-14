c015c86d5b48c37dc62624c485780649e6dc0ae6
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-13 23:41:32 -0500

    feat: render spinner when loading data

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <div class="h-screen flex flex-col">
-    <main class="w-full bg-pattern flex flex-col p-5 space-y-5">
+    <main class="w-full bg-pattern flex flex-col p-5 space-y-5 z-10">
       <header>
         <h1 class="text-white text-center font-bold text-2xl">
           IP Address Tracker
         </h1>
       </header>
       <IpInput v-model="ipAddress" @submit="handleIpInputSubmit" />
-      <ResponseBox
-        class="z-20"
-        :ip="responseInfo?.ip"
-        :city="responseInfo?.city"
-        :region="responseInfo?.region"
-        :postal-code="responseInfo?.postalCode"
-        :timezone="responseInfo?.timezone"
-        :isp="responseInfo?.isp"
-      />
+      <div
+        class="
+          bg-white
+          flex flex-col
+          justify-center
+          space-y-5
+          py-5
+          rounded-xl
+          z-10
+        "
+      >
+        <div v-if="isLoading" class="flex justify-center items-center">
+          <Spinner />
+        </div>
+        <div v-else>
+          <ResponseBox
+            class="z-20"
+            :ip="responseInfo?.ip"
+            :city="responseInfo?.city"
+            :region="responseInfo?.region"
+            :postal-code="responseInfo?.postalCode"
+            :timezone="responseInfo?.timezone"
+            :isp="responseInfo?.isp"
+          />
+        </div>
+      </div>
     </main>
     <div class="w-full h-full relative -mt-48">
       <div id="map" class="w-full h-full absolute z-0"></div>
@@ -32,6 +49,7 @@ import ResponseBox from './components/ResponseBox.vue';
 import { createMap } from './hooks/createMap';
 import { ApiResponse } from '@/types/types';
 import { mockApiCall } from '@/utils/mockApiCall';
+import Spinner from './components/Spinner.vue';
 
 const map = ref<Leaflet.Map>();
 const ipAddress = ref<string>();
@@ -66,8 +84,8 @@ const handleIpInputSubmit = async () => {
   responseInfo.value = response;
 
   map.value = createMap({
-    longitude: lng,
-    latitude: lat,
+    longitude: lng -1,
+    latitude: lat +1,
   });
   isLoading.value = false;
 };
