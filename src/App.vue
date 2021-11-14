4e357df17e04fdde9d61bb7e656997d0e5b670ea
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-14 18:36:18 -0500

    fix[#2]: change the-response-box component position between the map and the header (#4)
    * fix: import Spinner component and wrap data in a v-else
    
    * refactor(ResponseBox): prepend 'The' to the file name
    
    * fix(app): remove map wrapper negative top margin
    
    * refactor(the-response-box): make component absolute positioned
    
    I also wrapped the component in App.vue to set the relative parent closer to the component
    
    * refactor(app): make the-response-box component centered to its parent
    
    * refactor(app): increase height of <main>
    
    This was done to put the the-response-box to the desired position
    
    * refactor(the-response-box): move absolute class to the component instance in App.vue
    
    This makes the component isolated to any changes
    
    * refactor(app): wrap the-response-box with a relative div to make it respect the padding
    
    Finally I did this
    
    fix #2

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div class="h-screen flex flex-col">
-    <main class="w-full bg-pattern flex flex-col p-5 space-y-5">
+    <main class="w-full bg-pattern flex flex-col p-5 space-y-5 h-96">
       <header>
         <h1 class="text-white text-center font-bold text-2xl">
           IP Address Tracker
         </h1>
       </header>
       <IpInput v-model="ipAddress" @submit="handleIpInputSubmit" />
-      <ResponseBox
-        :is-loading="isLoading"
-        :ip="responseInfo?.ip"
-        :city="responseInfo?.city"
-        :region="responseInfo?.region"
-        :postal-code="responseInfo?.postalCode"
-        :timezone="responseInfo?.timezone"
-        :isp="responseInfo?.isp"
-      />
     </main>
-    <div class="w-full h-full relative -mt-48">
+    <div class="w-full px-5">
+      <div class="relative flex justify-center items-center">
+        <TheResponseBox
+          class="absolute w-full shadow-2xl"
+          :is-loading="isLoading"
+          :ip="responseInfo?.ip"
+          :city="responseInfo?.city"
+          :region="responseInfo?.region"
+          :postal-code="responseInfo?.postalCode"
+          :timezone="responseInfo?.timezone"
+          :isp="responseInfo?.isp"
+        />
+      </div>
+    </div>
+    <div class="w-full h-full relative">
       <div id="map" class="w-full h-full absolute z-0"></div>
     </div>
   </div>
@@ -28,7 +33,7 @@ import Leaflet from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { ref, onMounted } from 'vue';
 import IpInput from './components/IpInput.vue';
-import ResponseBox from './components/ResponseBox.vue';
+import TheResponseBox from './components/TheResponseBox.vue';
 import { createMap } from './hooks/createMap';
 import { ApiResponse } from '@/types/types';
 import { mockApiCall } from '@/utils/mockApiCall';
