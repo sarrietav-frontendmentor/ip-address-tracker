a3848565d27dcc67ca61459e2f93281360a6ac8b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-13 23:11:26 -0500

    refactor: create mock api call (#1)
    * feat: define mockApiCall
    
    * refactor: use generic type on promise
    
    * refactor: use mockApi
    
    While developing, to not use the remaining requests from Ipify
    
    * fix: change ipAddress ref value to user's ip

---
+++ src/utils/mockApiCall.ts
@@ -0,0 +1,20 @@
+import { ApiResponse } from '@/types/types';
+
+export const mockApiCall = (ipAddress?: string): Promise<ApiResponse> => {
+  return new Promise((resolve) => {
+    setTimeout(
+      () =>
+        resolve({
+          city: 'New york',
+          ip: ipAddress ?? '127.0.0.1',
+          isp: 'SpaceX Starlink',
+          lat: 40.7166638,
+          lng: -74.0,
+          postalCode: '10001',
+          region: 'NY',
+          timezone: '-05:00',
+        }),
+      1000
+    );
+  });
+};
