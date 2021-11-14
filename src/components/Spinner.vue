4311c8b0b8c9fa073f139b09aa7b3f83a89375d7
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-13 23:39:35 -0500

    fix(spinner): add border and border-t-color transparent

--- src/components/Spinner.vue
+++ src/components/Spinner.vue
@@ -1,5 +1,12 @@
 <template>
   <div
-    class="w-16 h-16 border-blue-500 border-solid rounded-full animate-spin"
+    style="border-top-color: transparent"
+    class="
+      w-16
+      h-16
+      border-4 border-blue-500 border-solid
+      rounded-full
+      animate-spin
+    "
   ></div>
 </template>
