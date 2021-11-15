b7ffcf48942b42701eb5955711e6664b8fe28316
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-15 13:19:15 -0500

    feat: make site responsive (#7)
    * refactor(app): increase header padding
    
    * refactor(app): make header font bigger and thinner
    
    Increase font-size to 3xl and font-weight to medium
    
    * style(app): format h1 class attr
    
    * refactor(app): increase the-response-box parent padding
    
    * refactor(the-response-box): extract TheResponseData component
    
    * refactor(response-box-data): remove 'The' from the file name
    
    Following the Vue style guide:
    https://v3.vuejs.org/style-guide/#single-instance-component-names-strongly-recommended
    
    * feat(the-response-box): make response-box-data container a grid container
    
    * feat(the-response-box): align items to the start
    
    * feat(response-box-data): align text data to the left
    
    * refactor(the-response-box): strech items across the grid
    
    * refactor(response-box-data): justify items to the start on the lg breakpoint
    
    * fix(the-response-box): delete response-box-data's parent div
    
    I forgot to remove those divs when I was refactoring this component
    
    * fix(response-box-data): change p-6 to work only on large breakpoints
    
    * style(response-box-data): format code
    
    * feat(the-response-box): add divider to the response-box-data parent
    
    * fix(app): background repeating itself
    
    Used bg-cover

---
+++ src/components/ResponseBoxData.vue
@@ -0,0 +1,18 @@
+<template>
+  <div
+    class="
+      flex flex-col
+      justify-center
+      lg:justify-start lg:items-start
+      space-y-1
+      lg:p-6
+    "
+  >
+    <span class="text-center text-xs text-gray-500 font-bold uppercase">
+      <slot name="header"></slot>
+    </span>
+    <span class="text-center lg:text-left text-2xl text-gray-800 font-bold">
+      <slot name="data"></slot>
+    </span>
+  </div>
+</template>
