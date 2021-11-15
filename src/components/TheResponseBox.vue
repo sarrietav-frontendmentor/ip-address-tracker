18a2fe34991d8c462f4fe06e7188c30932b23ae9
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-15 12:17:14 -0500

    refactor(the-response-box): strech items across the grid

--- src/components/TheResponseBox.vue
+++ src/components/TheResponseBox.vue
@@ -5,7 +5,7 @@
     <div v-if="isLoading" class="flex justify-center items-center">
       <TheSpinner />
     </div>
-    <div v-else class="lg:grid grid-cols-4 items-start">
+    <div v-else class="lg:grid grid-cols-4 items-strech">
       <ResponseBoxData>
         <template #header>Ip</template>
         <template #data> {{ ip }} </template>
