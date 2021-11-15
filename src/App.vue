aff98c52bcf4c9916f9129a8789efaf02c2e9728
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-15 00:24:59 -0500

    refactor(app): increase header padding

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <main class="h-screen flex flex-col">
-    <header class="w-full bg-pattern flex flex-col p-5 space-y-5 pb-36">
+    <header
+      class="w-full bg-pattern flex flex-col p-5 lg:px-[30rem] space-y-5 pb-36"
+    >
       <h1 class="text-white text-center font-bold text-2xl">
         IP Address Tracker
       </h1>
