9f8b8bdec05223c1e1001a745f4facea8976a351
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-15 18:34:51 -0500

    style(response-box-data): format file

--- src/components/ResponseBoxData.vue
+++ src/components/ResponseBoxData.vue
@@ -8,7 +8,15 @@
       lg:p-6
     "
   >
-    <span class="text-center text-xs tracking-widest text-gray-500 font-bold uppercase">
+    <span
+      class="
+        text-center text-xs
+        tracking-widest
+        text-gray-500
+        font-bold
+        uppercase
+      "
+    >
       <slot name="header"></slot>
     </span>
     <span class="text-center lg:text-left text-2xl text-gray-800 font-bold">
