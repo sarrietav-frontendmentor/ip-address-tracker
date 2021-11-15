6e00d4d4896cdfd3c6e30baf024fc0f77c9c248c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-15 13:01:35 -0500

    style(response-box-data): format code

--- src/components/ResponseBoxData.vue
+++ src/components/ResponseBoxData.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="flex flex-col justify-center lg:justify-start lg:items-start space-y-1 lg:p-6">
+  <div
+    class="
+      flex flex-col
+      justify-center
+      lg:justify-start lg:items-start
+      space-y-1
+      lg:p-6
+    "
+  >
     <span class="text-center text-xs text-gray-500 font-bold uppercase">
       <slot name="header"></slot>
     </span>
