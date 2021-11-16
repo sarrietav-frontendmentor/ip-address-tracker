90d3d214045557242c0c0f3b321827f969e2a9e1
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-16 00:45:44 -0500

    feat: use Ipapi API to get geolocation data (#17)
    * refactor(geoapi): change api response interface
    
    * refactor(ipGeo): reduce interface to needed types
    
    * refactor: map response value to return values
    
    * refactor: remove axios params
    
    * refactor: change url
    
    * feat: add ipAddress if used
    
    * feat: use the api instead of the mock api

--- src/services/ipGeolocationApi.ts
+++ src/services/ipGeolocationApi.ts
@@ -1,55 +1,43 @@
 import { ApiResponse } from '@/types/types';
 import axios from 'axios';
 
-interface IpifyResponse {
-  ip: string;
-  location: {
-    country: string;
-    region: string;
-    city: string;
-    lat: number;
-    lng: number;
-    postalCode: string;
-    timezone: string;
-    geonameId: number;
-  };
-  domains: string[];
-  as: {
-    asn: number;
-    name: string;
-    route: string;
-    domain: string;
-    type: string;
-  };
-  isp: string;
+interface IpapiResponse {
+  ip: '8.8.8.8';
+  city: 'Mountain View';
+  region_code: 'CA';
+  postal: '94035';
+  latitude: 37.386;
+  longitude: -122.0838;
+  utc_offset: '-0800';
+  org: 'Google LLC';
 }
 
 export const callGeolocationApi = async (
   ipAddress?: string
 ): Promise<ApiResponse> => {
-  const response = await axios.get<IpifyResponse>(
-    'https://geo.ipify.org/api/v2/country,city',
-    {
-      params: ipAddress
-        ? { apiKey: process.env.VUE_APP_IPIFY_API_KEY, ipAddress }
-        : { apiKey: process.env.VUE_APP_IPIFY_API_KEY },
-    }
+  const response = await axios.get<IpapiResponse>(
+    `https://ipapi.co/${ipAddress ? ipAddress + '/' : ''}json/`
   );
 
   const {
     ip,
-    location: { city, region, lat, lng, postalCode, timezone },
-    isp,
+    city,
+    org,
+    latitude,
+    longitude,
+    postal,
+    region_code,
+    utc_offset,
   } = response.data;
 
   return {
     ip,
     city,
-    region,
-    lat,
-    lng,
-    postalCode,
-    timezone,
-    isp,
+    region: region_code,
+    lat: latitude,
+    lng: longitude,
+    postalCode: postal,
+    timezone: utc_offset,
+    isp: org,
   };
 };
