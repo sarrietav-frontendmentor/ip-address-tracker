bac083adb48ffd2ac15c3aacc1087ea903239a9d
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-15 22:09:02 -0500

    refactor(hooks): create handleApi file

---
+++ src/hooks/handleApi.ts
@@ -0,0 +1,22 @@
+import { Map } from 'leaflet';
+import { ApiResponse } from '@/types/types';
+import { mockApiCall } from '@/utils/mockApiCall';
+import { createMap } from './createMap';
+
+export const handleApiResponse = async (
+  ipAddress?: string
+): Promise<{ response: ApiResponse; leafletMap: Map }> => {
+  const response = await mockApiCall(ipAddress);
+
+  const { lng, lat } = response;
+
+  const leafletMap = createMap({
+    longitude: lng,
+    latitude: lat,
+  });
+
+  return {
+    response,
+    leafletMap,
+  };
+};
