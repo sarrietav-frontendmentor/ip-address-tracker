a2130bff3e62fa7ec71bfd45b21db3ad3019a4b7
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-16 00:24:14 -0500

    refactor(geoapi): change api response interface

--- src/services/ipGeolocationApi.ts
+++ src/services/ipGeolocationApi.ts
@@ -1,33 +1,31 @@
 import { ApiResponse } from '@/types/types';
 import axios from 'axios';
 
-interface IpifyResponse {
-  ip: string;
-  location: {
-    country: string;
-    region: string;
-    city: string;
-    lat: number;
-    lng: number;
-    postalCode: string;
-    timezone: string;
-    geonameId: number;
-  };
-  domains: string[];
-  as: {
-    asn: number;
-    name: string;
-    route: string;
-    domain: string;
-    type: string;
-  };
-  isp: string;
+interface IpapiResponse {
+  ip: '208.67.222.222';
+  city: 'San Francisco';
+  region: 'California';
+  region_code: 'CA';
+  country: 'US';
+  country_name: 'United States';
+  continent_code: 'NA';
+  in_eu: false;
+  postal: '94107';
+  latitude: 37.7697;
+  longitude: -122.3933;
+  timezone: 'America/Los_Angeles';
+  utc_offset: '-0800';
+  country_calling_code: '+1';
+  currency: 'USD';
+  languages: 'en-US,es-US,haw,fr';
+  asn: 'AS36692';
+  org: 'OpenDNS, LLC';
 }
 
 export const callGeolocationApi = async (
   ipAddress?: string
 ): Promise<ApiResponse> => {
-  const response = await axios.get<IpifyResponse>(
+  const response = await axios.get<IpapiResponse>(
     'https://geo.ipify.org/api/v2/country,city',
     {
       params: ipAddress
