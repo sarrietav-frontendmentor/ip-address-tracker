2817622f5b6bc5b871ccaeade10fe32f67f23e05
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-17 12:40:43 -0500

    Fix text overflow in TheResponseBox component (#20)
    Fixes #18

--- src/components/ResponseBoxData.vue
+++ src/components/ResponseBoxData.vue
@@ -5,6 +5,7 @@
       justify-center
       lg:justify-start lg:items-start
       space-y-2
+      px-5
       lg:p-6
     "
   >
@@ -19,7 +20,17 @@
     >
       <slot name="header"></slot>
     </span>
-    <span class="text-center lg:text-left text-2xl text-gray-800 font-bold">
+    <span
+      class="
+        text-center
+        lg:text-left
+        text-2xl text-gray-800
+        font-bold
+        overflow-ellipsis
+        whitespace-nowrap
+        overflow-hidden
+      "
+    >
       <slot name="data"></slot>
     </span>
   </div>
