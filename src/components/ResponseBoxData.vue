907b1751765791e334b9520a5e88e7801386ee02
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-17 12:41:27 -0500

    Revert "Fix text overflow in TheResponseBox component (#20)"
    This reverts commit 2817622f5b6bc5b871ccaeade10fe32f67f23e05.

--- src/components/ResponseBoxData.vue
+++ src/components/ResponseBoxData.vue
@@ -5,7 +5,6 @@
       justify-center
       lg:justify-start lg:items-start
       space-y-2
-      px-5
       lg:p-6
     "
   >
@@ -20,17 +19,7 @@
     >
       <slot name="header"></slot>
     </span>
-    <span
-      class="
-        text-center
-        lg:text-left
-        text-2xl text-gray-800
-        font-bold
-        overflow-ellipsis
-        whitespace-nowrap
-        overflow-hidden
-      "
-    >
+    <span class="text-center lg:text-left text-2xl text-gray-800 font-bold">
       <slot name="data"></slot>
     </span>
   </div>
