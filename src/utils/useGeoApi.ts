7e12ae165dbd3f4dd992604ac7e4e168def80d4a
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-17 16:27:38 -0500

    refactor(useGeoApi): use Ipapi instead of mock

--- src/utils/useGeoApi.ts
+++ src/utils/useGeoApi.ts
@@ -1,12 +1,12 @@
 import { Map } from 'leaflet';
 import { ApiResponse } from '@/types/types';
 import { createMap } from './createMap';
-import { mockApiCall } from './mockApiCall';
+import { callGeolocationApi } from '@/services/ipGeolocationApi';
 
 export const useGeoApi = async (
   ipAddress?: string
 ): Promise<{ response: ApiResponse; leafletMap: Map }> => {
-  const response = await mockApiCall(ipAddress);
+  const response = await callGeolocationApi(ipAddress);
 
   const { lng, lat } = response;
 
