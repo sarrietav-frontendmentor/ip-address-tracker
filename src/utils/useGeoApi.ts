aca1586b04425ae1d96b9206cd0f04c1ea76dda7
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2021-11-20 13:08:01 -0500

    Change Leaflet map view instead of removing it (#27)
    Fixes #26

--- src/utils/useGeoApi.ts
+++ src/utils/useGeoApi.ts
@@ -1,19 +1,24 @@
-import { Map } from 'leaflet';
+import { LatLng, Map } from 'leaflet';
 import { ApiResponse } from '@/types/types';
 import { createMap } from './createMap';
 import { callGeolocationApi } from '@/services/ipGeolocationApi';
 
 export const useGeoApi = async (
-  ipAddress?: string
+  ipAddress?: string,
+  leafletMap?: Map
 ): Promise<{ response: ApiResponse; leafletMap: Map }> => {
   const response = await callGeolocationApi(ipAddress);
 
   const { lng, lat } = response;
 
-  const leafletMap = createMap({
-    longitude: lng,
-    latitude: lat,
-  });
+  if (leafletMap) {
+    leafletMap.setView(new LatLng(lat, lng), 15);
+  } else {
+    leafletMap = createMap({
+      longitude: lng,
+      latitude: lat,
+    });
+  }
 
   return {
     response,
